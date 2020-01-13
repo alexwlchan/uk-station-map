a5c438be82db9569f28a09ad5d2ae029645e1180
Alex Chan <alex@alexwlchan.net>
2020-01-13 20:40:00 +0000

    Add JavaScript functions to add/remove stations

---
+++ static/map_manager.js
@@ -0,0 +1,81 @@
+L.TileLayer.Grayscale = L.TileLayer.extend({
+	options: {
+		quotaRed: 21,
+		quotaGreen: 71,
+		quotaBlue: 8,
+		quotaDividerTune: 0,
+		quotaDivider: function() {
+			return this.quotaRed + this.quotaGreen + this.quotaBlue + this.quotaDividerTune;
+		}
+	},
+
+	initialize: function (url, options) {
+		options = options || {}
+    options.crossOrigin = true;
+		L.TileLayer.prototype.initialize.call(this, url, options);
+
+		this.on('tileload', function(e) {
+			this._makeGrayscale(e.tile);
+		});
+	},
+
+	_createTile: function () {
+		var tile = L.TileLayer.prototype._createTile.call(this);
+    tile.crossOrigin = "Anonymous";
+		return tile;
+	},
+
+	_makeGrayscale: function (img) {
+		if (img.getAttribute('data-grayscaled'))
+			return;
+
+    img.crossOrigin = '';
+		var canvas = document.createElement("canvas");
+		canvas.width = img.width;
+		canvas.height = img.height;
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(img, 0, 0);
+
+		var imgd = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		var pix = imgd.data;
+		for (var i = 0, n = pix.length; i < n; i += 4) {
+                        pix[i] = pix[i + 1] = pix[i + 2] = (this.options.quotaRed * pix[i] + this.options.quotaGreen * pix[i + 1] + this.options.quotaBlue * pix[i + 2]) / this.options.quotaDivider();
+		}
+		ctx.putImageData(imgd, 0, 0);
+		img.setAttribute('data-grayscaled', true);
+		img.src = canvas.toDataURL();
+	}
+});
+
+class MapManager {
+  constructor(div_id) {
+    this.map = L.map(div_id);
+    this.addOpenstreetMapTiles();
+
+    this.markerCache = {};
+  }
+
+  addOpenstreetMapTiles() {
+    this.map.setView([54.505, -4.5], 6);
+
+    new L.TileLayer.Grayscale('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, station locations from <a href="https://transportapi.com/">TransportAPI</a>',
+    }).addTo(this.map);
+  }
+
+  addMarker(label, lat, long) {
+    const marker = L.circle([lat, long], {
+      fill: true,
+      color: '#0b9e00',
+      fillColor: 'rgba(11, 158, 0, 0.3)',
+      radius: 7500,
+    }).addTo(this.map);
+
+    this.markerCache[label] = marker;
+  }
+
+  removeMarker(label) {
+    this.map.removeLayer(this.markerCache[label]);
+    delete this.markerCache[label];
+  }
+}
