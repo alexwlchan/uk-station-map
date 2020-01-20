99dbeb04779eecd6dff89cbf3f5e77438857a8a0
Alex Chan <alex@alexwlchan.net>
2020-01-20 22:44:06 +0000

    Create minified styles in a consistent order

--- scripts/minify_assets.py
+++ scripts/minify_assets.py
@@ -9,7 +9,7 @@ import jsmin
 if __name__ == "__main__":
     css_strings = []
 
-    for css_path in glob.iglob("assets/*.css"):
+    for css_path in sorted(glob.glob("assets/*.css")):
         css_strings.append(cssmin.cssmin(open(css_path).read()))
 
     with open("static/stations.min.css", "w") as out_file:
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     js_strings = []
 
-    for js_path in glob.iglob("assets/*.js"):
+    for js_path in sorted(glob.glob("assets/*.js")):
         js_strings.append(jsmin.jsmin(open(js_path).read()))
 
     with open("static/stations.min.js", "w") as out_file:
